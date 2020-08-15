61985d65b0e643fca12d1b3347b08549991ad021
Vinicio <viniciojoj@gmail.com>
2020-08-14 20:26:06 -0600

    Cambios al login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,6 +54,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
+                                    Login                                    
                                     {{ __('Login') }}
                                 </button>
 
