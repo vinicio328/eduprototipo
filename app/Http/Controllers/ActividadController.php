d4bf09bf7d58335ece11c76f8343ab574423c6b6
Vinicio <viniciojoj@gmail.com>
2020-08-16 00:33:47 -0600

    Se agrego guard a los nuevos controladores

--- app/Http/Controllers/ActividadController.php
+++ app/Http/Controllers/ActividadController.php
@@ -9,6 +9,16 @@ use App\Actividad;
 class ActividadController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
      * Display a listing of the resource.
      *
      * @param  int  $curso_id
--- app/Http/Controllers/AsignacionController.php
+++ app/Http/Controllers/AsignacionController.php
@@ -9,6 +9,16 @@ use App\Estudiante;
 class AsignacionController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
      * Display a listing of the resource.
      *
      * @param  int  $estudiante_id
